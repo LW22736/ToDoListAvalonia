e40b60704874856474f0a419df365a19e13e3c63
Thomas Parr <github.us8r1@tpalias.com>
2024-01-23 16:30:04 +0000

    Finished tutorial for ToDoList App (https://docs.avaloniaui.net/docs/tutorials/todo-list-app/)

--- ToDoList/ViewModels/MainWindowViewModel.cs
+++ ToDoList/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,50 @@
-﻿namespace ToDoList.ViewModels;
+﻿using ReactiveUI;
+using System;
+using System.Reactive.Linq;
+using ToDoList.DataModel;
+using ToDoList.Services;
 
-public class MainWindowViewModel : ViewModelBase
+namespace ToDoList.ViewModels
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    public class MainWindowViewModel : ViewModelBase
+    {
+        private ViewModelBase _contentViewModel;
+
+        //this has a dependency on the ToDoListService
+
+        public MainWindowViewModel()
+        {
+            var service = new ToDoListService();
+            ToDoList = new ToDoListViewModel(service.GetItems());
+            _contentViewModel = ToDoList;
+        }
+
+        public ViewModelBase ContentViewModel
+        {
+            get => _contentViewModel;
+            private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
+        }
+
+        public ToDoListViewModel ToDoList { get; }
+
+        public void AddItem()
+        {
+            AddItemViewModel addItemViewModel = new();
+
+            Observable.Merge(
+                addItemViewModel.OkCommand,
+                addItemViewModel.CancelCommand.Select(_ => (ToDoItem?)null))
+                .Take(1)
+                .Subscribe(newItem =>
+                {
+                    if (newItem != null)
+                    {
+                        ToDoList.ListItems.Add(newItem );
+                    }
+                    ContentViewModel = ToDoList;
+                });
+
+            ContentViewModel = addItemViewModel;
+        }
+    }
 }
